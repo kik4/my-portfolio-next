699066423d3b9f32f1eeee13ab9dae1b3214cca9
kik4 <kik4.contact@gmail.com>
2024-10-28 23:01:59 +0900

    use webfont

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,7 @@
+/* eslint-disable @next/next/no-page-custom-font */
 import type { Metadata } from "next";
-import { Lato, Noto_Sans_JP } from "next/font/google";
 import "./globals.css";
 import clsx from "clsx";
-
-const lato = Lato({
-	subsets: ["latin"],
-	weight: ["400", "700"],
-	variable: "--font-lato",
-});
-
-const notoSansJp = Noto_Sans_JP({
-	subsets: ["latin"],
-	variable: "--font-noto-sans-jp",
-});
 
 export const metadata: Metadata = {
 	title: "kik4.work",
@@ -26,15 +15,13 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="ja">
-			<body
-				className={clsx(
-					notoSansJp.variable,
-					lato.variable,
-					"font-default antialiased",
-				)}
-			>
-				{children}
-			</body>
+			<head>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Noto+Sans+JP:wght@400;700&display=swap"
+					rel="stylesheet"
+				/>
+			</head>
+			<body className={clsx("antialiased")}>{children}</body>
 		</html>
 	);
 }
