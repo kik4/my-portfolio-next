4ec6d629488a2fe3af6b2a6e26c4a6bf33d8348a
kik4 <kik4.contact@gmail.com>
2025-10-21 22:50:22 +0900

    Change width of articles

--- app/algorithm/_components/InPageLayout.tsx
+++ app/algorithm/_components/InPageLayout.tsx
@@ -27,7 +27,7 @@ export function InPageLayout({
 
       {/* Article Content */}
       <div className="mb-16 flex-1 overflow-y-auto bg-white px-8 py-8 dark:bg-gray-800">
-        <div className="mx-auto max-w-4xl [&_h1]:py-2 [&_h1]:font-bold [&_h1]:text-2xl [&_h2]:mt-6 [&_h2]:py-1 [&_h2]:font-bold [&_h2]:text-xl [&_p]:py-2">
+        <div className="mx-auto max-w-3xl [&_h1]:py-2 [&_h1]:font-bold [&_h1]:text-2xl [&_h2]:mt-6 [&_h2]:py-1 [&_h2]:font-bold [&_h2]:text-xl [&_p]:py-2">
           {children}
         </div>
       </div>
